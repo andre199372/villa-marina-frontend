c0f90f1be559c1fbcab1c216f00bc3afd089e247
andre199372 <giovanniantonino23@gmail.com>
2026-01-24 16:26:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1909,7 +1909,7 @@ className={`px-4 py-2 rounded-lg transition capitalize ${
         
         <div className="h-[500px] w-full">
           <iframe 
-            src="https://www.google.com/maps/place/Via+Monte+Bianco,+4,+58043+Castiglione+della+Pescaia+GR/@42.766572,10.8753447,17z/data=!4m6!3m5!1s0x1329ceda3d6dbe3b:0x8515901c95071347!8m2!3d42.7665602!4d10.8777694!16s%2Fg%2F11csnrh755?entry=ttu&g_ep=EgoyMDI2MDEyMS4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D"
+            src="<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1464.5091529402443!2d10.876487300310075!3d42.766818120117314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sit!2sit!4v1769268280126!5m2!1sit!2sit"
             width="100%" 
             height="100%" 
             style={{ border: 0 }}
